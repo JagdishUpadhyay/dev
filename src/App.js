6eb269aeb20d77f4ed6d39695e53eb0577b1dcfc
Jagdish <jagadishupadhyay89@gmail.com>
2019-10-04 14:59:48 +0530

    conflict resolved

--- src/App.js
+++ src/App.js
@@ -49,35 +49,7 @@ export default class App extends React.Component {
         </div>
         </div>
         </div>
-        </div>
-        {/* <div className="container-fluid">
-        <Form>
-          <Form.Group controlId="formName">
-            <Form.Label>Employee Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Name" />
-          </Form.Group>
-
-          <Form.Group controlId="formId">
-            <Form.Label>Employee Id</Form.Label>
-            <Form.Control type="text" placeholder="Employee Id" />
-          </Form.Group>
-
-          <Form.Group controlId="formEmail">
-            <Form.Label>Employee Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter Email" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <div className="row" style={{display: "flex", flex:1}}>
-            <div style={{flex:1 }}>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-            </div>
-          </div>
-        </div>
+      </div>
       </div>
     );
   }
