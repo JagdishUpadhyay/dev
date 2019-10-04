5f683a5526fd3f2456b59155ec525654611b21b8
Jagdish <jagadishupadhyay89@gmail.com>
2019-10-04 10:38:56 +0530

    react router navigation added.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import FormComponent from './FormComponent';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(
-    <div><App /></div>,
-    document.getElementById('root')
-);
+ReactDOM.render((
+    <BrowserRouter>
+      <div>
+          <Route exact path="/" component={App} />
+          <Route path="/details" component={FormComponent} />
+      </div>
+    </BrowserRouter>
+  ), document.getElementById('root'))
+
+// ReactDOM.render(
+//     <div><App /></div>,
+//     document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
